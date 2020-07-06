bef55aab5923a25f2d21c88235bf4e5938573610
HuskyRye <1412167533@qq.com>
2020-07-06 18:19:58 +0800

    disc02

---
+++ disc/disc02.java
@@ -0,0 +1,43 @@
+public class IntList {
+    int first;
+    IntList rest;
+
+    public static IntList square(IntList L) {
+        if (L == null)
+            return null;
+        return new IntList(L.first * L.first, square(L.rest));
+    }
+
+    public static IntList squareDestructive(IntList L) {
+        IntList p = L;
+        while (p != null) {
+            p.first *= p.first;
+            p = p.rest;
+        }
+        return L;
+    }
+
+    public static IntList squareIteratively(IntList L) {
+        if (L == null)
+            return L;
+        IntList start = IntList(L.first * L.first, null);
+        IntList last = start;
+        IntList p = L.rest;
+        while (p != null) {
+            last.rest = IntList(p.first * p.first, null);
+            last = last.rest;
+            p = p.rest;
+        }
+        return start;
+    }
+
+    public static IntList squareDestructiveRecursively(IntList L) {
+        if (L == null)
+            return null;
+        else {
+            L.first *= L.first;
+            squareDestructiveRecursively(L.rest);
+        }
+        return L;
+    }
+}
