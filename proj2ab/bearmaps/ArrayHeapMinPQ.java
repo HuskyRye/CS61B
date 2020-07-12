bd1ff7b6b97b3eb98ae2e71dcb7ca35f4506d3b1
HuskyRye <1412167533@qq.com>
2020-07-13 00:15:24 +0800

    proj2a

---
+++ proj2ab/bearmaps/ArrayHeapMinPQ.java
@@ -0,0 +1,136 @@
+package bearmaps;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.NoSuchElementException;
+
+public class ArrayHeapMinPQ<T> implements ExtrinsicMinPQ<T> {
+    private class Node{
+        private T item;
+        private double priority;
+        Node(T item, double priority) {
+            this.item = item;
+            this.priority = priority;
+        }
+    }
+
+    private ArrayList<Node> heap;
+    private HashMap<T, Integer> items;
+
+    public ArrayHeapMinPQ() {
+        heap = new ArrayList<>();
+        heap.add(new Node(null, -1.0 / 0.0));
+        items = new HashMap<>();
+    }
+
+    private void swim(int k) {
+        Node parent = heap.get(k / 2);
+        Node self = heap.get(k);
+        if (parent.priority > self.priority) {
+            heap.set(k / 2, self);
+            items.put(self.item, k / 2);
+            heap.set(k, parent);
+            items.put(parent.item, k);
+            swim(k / 2);
+        }
+    }
+
+    @Override
+    public void add(T item, double priority) {
+        if (contains(item)) {
+            throw new IllegalArgumentException();
+        }
+        heap.add(new Node(item, priority));
+        items.put(item, items.size() + 1);
+        swim(items.size());
+    }
+
+    @Override
+    public boolean contains(T item) {
+        return items.containsKey(item);
+    }
+
+    @Override
+    public T getSmallest() {
+        if (items.isEmpty()) {
+            throw new NoSuchElementException();
+        }
+        return heap.get(1).item;
+    }
+
+    private void sink(int k) {
+        Node self = heap.get(k);
+        int index1 = 2 * k;
+        int index2 = 2 * k + 1;
+        Node min;
+        int index;
+        if (index2 <= items.size()) {
+            Node left = heap.get(index1);
+            Node right = heap.get(index2);
+            if (left.priority < right.priority) {
+                min = left;
+                index = index1;
+            } else {
+                min = right;
+                index = index2;
+            }
+        } else if (index1 <= items.size()) {
+            min = heap.get(index1);
+            index = index1;
+        } else {
+            return;
+        }
+
+        if (self.priority > min.priority) {
+            heap.set(k, min);
+            items.put(min.item, k);
+            heap.set(index, self);
+            items.put(self.item, index);
+            sink(index);
+        }
+    }
+
+    @Override
+    public T removeSmallest() {
+        if (items.isEmpty()) {
+            throw new NoSuchElementException();
+        }
+        Node min = heap.get(1);
+        Node last = heap.get(items.size());
+        if (min == last) {
+            heap.remove(1);
+            items.remove(min.item);
+            return min.item;
+        }
+        heap.remove(items.size());
+        heap.set(1, last);
+        items.put(last.item, 1);
+        items.remove(min.item);
+        sink(1);
+        return min.item;
+    }
+
+    @Override
+    public int size() {
+        return items.size();
+    }
+
+
+    /* Changes the priority of the given item. Throws NoSuchElementException if the item
+     * doesn't exist. */
+    @Override
+    public void changePriority(T item, double priority) {
+        if (!contains(item)) {
+            throw new NoSuchElementException();
+        }
+        int index = items.get(item);
+        Node self = heap.get(index);
+        double oldpriority = self.priority;
+        self.priority = priority;
+        if (oldpriority > priority) {
+            swim(index);
+        } else {
+            sink(index);
+        }
+    }
+}
--- proj2ab/bearmaps/ArrayHeapMinPQTest.java
+++ proj2ab/bearmaps/ArrayHeapMinPQTest.java
@@ -2,6 +2,67 @@ package bearmaps;
 
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class ArrayHeapMinPQTest {
+    @Test
+    public void basicTest() {
+        ArrayHeapMinPQ<String> pq = new ArrayHeapMinPQ<>();
+        assertEquals(0, pq.size());
+        for (int i = 1; i <= 100; ++i) {
+            pq.add(String.valueOf(i), i);
+            assertEquals(i, pq.size());
+        }
+        for (int i = 1; i <= 100; ++i) {
+            assertTrue(pq.contains(String.valueOf(i)));
+            assertEquals(String.valueOf(i), pq.getSmallest());
+            assertEquals(String.valueOf(i), pq.removeSmallest());
+        }
+
+        pq = new ArrayHeapMinPQ<>();
+        for (int i = 100; i >= 1; --i) {
+            pq.add(String.valueOf(i), i);
+        }
+        for (int i = 1; i <= 100; ++i) {
+            assertTrue(pq.contains(String.valueOf(i)));
+            assertEquals(String.valueOf(i), pq.getSmallest());
+            assertEquals(String.valueOf(i), pq.removeSmallest());
+        }
+    }
+
+    @Test
+    public void changePriorityTest() {
+        ArrayHeapMinPQ<String> pq = new ArrayHeapMinPQ<>();
+        for (int i = 1; i <= 100; ++i) {
+            pq.add(String.valueOf(i), 101);
+        }
+        for (int i = 1; i <= 100; ++i) {
+            pq.changePriority(String.valueOf(i), 101 - i);
+            assertEquals(String.valueOf(i), pq.getSmallest());
+        }
+    }
+
+    @Test
+    public void timeTest() {
+        ArrayHeapMinPQ<String> pq = new ArrayHeapMinPQ<>();
+        NaiveMinPQ<String> npq = new NaiveMinPQ<>();
+        for (int i = 1; i <= 1000000; ++i) {
+            pq.add(String.valueOf(i), i);
+            npq.add(String.valueOf(i), i);
+        }
+        int N = 1000;
+        long start = System.currentTimeMillis();
+        for (int i = 1; i <= N; ++i) {
+            pq.removeSmallest();
+        }
+        long end = System.currentTimeMillis();
+        System.out.println("ArrayHeapMinPQ: " + (end - start)/1000.0 +  " seconds.");
+
+        start = System.currentTimeMillis();
+        for (int i = 1; i <= N; ++i) {
+            npq.removeSmallest();
+        }
+        end = System.currentTimeMillis();
+        System.out.println("NaiveMinPQ: " + (end - start)/1000.0 +  " seconds.");
+    }
 }
